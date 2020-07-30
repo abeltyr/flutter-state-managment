17f8bc01b56a7f117e0bf2707612df4fc0eaf2a5
avell21 <abeltyr@gmail.com>
2020-07-30 20:53:53 +0300

    update on way of fetching data

--- lib/widget/productView.dart
+++ lib/widget/productView.dart
@@ -6,40 +6,39 @@ import "../provider/product.dart";
 class ProductView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(
-      context
-    );
-    return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(
-          context,
-          ProductDetail.routeName,
-          arguments: product.id,
-        );
-      },
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(15),
-        child: GridTile(
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.fill,
-          ),
-          footer: GridTileBar(
-            backgroundColor: Colors.black54,
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
+    return Consumer<Product>(
+      builder: (ctx, product, child) => GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(
+            context,
+            ProductDetail.routeName,
+            arguments: product.id,
+          );
+        },
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: GridTile(
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.fill,
             ),
-            leading: IconButton(
-              icon: Icon(
-                  product.favorite ? Icons.favorite : Icons.favorite_border),
-              onPressed: () {
-                product.addToFavorite();
-              },
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.shopping_cart),
-              onPressed: () {},
+            footer: GridTileBar(
+              backgroundColor: Colors.black54,
+              title: Text(
+                product.title,
+                textAlign: TextAlign.center,
+              ),
+              leading: IconButton(
+                icon: Icon(
+                    product.favorite ? Icons.favorite : Icons.favorite_border),
+                onPressed: () {
+                  product.addToFavorite();
+                },
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.shopping_cart),
+                onPressed: () {},
+              ),
             ),
           ),
         ),
