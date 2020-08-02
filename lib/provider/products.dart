fa33d1ab29bca4d34e1839a54e35816395df5540
avell21 <abeltyr@gmail.com>
2020-08-02 20:41:53 +0300

    more on http

--- lib/provider/products.dart
+++ lib/provider/products.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'product.dart';
 import 'package:http/http.dart' as http;
-import 'package:intl/intl.dart';
+import '../model/httpError.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = [];
@@ -12,24 +12,9 @@ class Products with ChangeNotifier {
   }
 
   Future<void> getItems() async {
-    // http
-    //     .get(url)
-    //     .then(
-    //       (res) => {
-    //         print(
-    //           json.decode(res.body),
-    //         ),
-    //         _items = json.decode(res.body)
-    //       },
-    //     )
-    //     .catchError(
-    //       (onError) => {
-    //         print(onError),
-    //       },
-    //     );
     try {
-      // const url = "http://10.0.2.2:5100/products";
-      const url = "https://acc6d527438b.ngrok.io/products";
+      const url = "http://10.0.2.2:5100/products";
+      // const url = "https://acc6d527438b.ngrok.io/products";
       final data = await http.get(url);
       final extractedData = json.decode(data.body) as List<dynamic>;
       List<Product> loadedProducts = [];
@@ -41,10 +26,6 @@ class Products with ChangeNotifier {
             title: prodData["title"],
             description: prodData["description"],
             imageUrl: prodData["imageUrl"],
-            // createdAt:
-            //     DateFormat('dd/MM/yyyy hh:mm').format(prodData["createdAt"]),
-            // updatedAt:
-            //     DateFormat('dd/MM/yyyy hh:mm').format(prodData["updatedAt"]),
           ),
         );
       });
@@ -59,67 +40,77 @@ class Products with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  Future<void> addProduct(value) {
-    // const url = "http://10.0.2.2:5100/product";
-    // http.post(
-    //   url,
-    //   body: json.encode(
-    //     {
-    //       "title": 'Trousers',
-    //       "description": 'A nice pair of trousers.',
-    //       "price": 59.99,
-    //       "imageUrl":
-    //           'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    //     },
-    //   ),
-    // );
-    // _items.add(value);
-    return Future.value();
-    notifyListeners();
-  }
-
-
-  Future<void> updateProducts() async {
-    // http
-    //     .get(url)
-    //     .then(
-    //       (res) => {
-    //         print(
-    //           json.decode(res.body),
-    //         ),
-    //         _items = json.decode(res.body)
-    //       },
-    //     )
-    //     .catchError(
-    //       (onError) => {
-    //         print(onError),
-    //       },
-    //     );
+  Future<void> addProduct() async {
     try {
-      // const url = "http://10.0.2.2:5100/products";
-      const url = "https://acc6d527438b.ngrok.io/products";
-      final data = await http.get(url);
-      final extractedData = json.decode(data.body) as List<dynamic>;
-      List<Product> loadedProducts = [];
-      extractedData.forEach((prodData) {
-        loadedProducts.add(
-          Product(
-            id: prodData["id"],
-            price: prodData["price"],
-            title: prodData["title"],
-            description: prodData["description"],
-            imageUrl: prodData["imageUrl"],
-            // createdAt:
-            //     DateFormat('dd/MM/yyyy hh:mm').format(prodData["createdAt"]),
-            // updatedAt:
-            //     DateFormat('dd/MM/yyyy hh:mm').format(prodData["updatedAt"]),
-          ),
-        );
-      });
-      _items = loadedProducts;
+      const url = "http://10.0.2.2:5100/product";
+      // const url = "https://acc6d527438b.ngrok.io/product";
+      var added = await http.post(
+        url,
+        body: json.encode(
+          {
+            "title": 'Trousers',
+            "description": 'A nice pair of trousers.',
+            "price": 59.99,
+            "imageUrl":
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+          },
+        ),
+      );
+
+      final extractedData = json.decode(added.body) as Map<String, dynamic>;
+      _items.add(
+        Product(
+          id: extractedData["id"],
+          price: extractedData["price"],
+          title: extractedData["title"],
+          description: extractedData["description"],
+          imageUrl: extractedData["imageUrl"],
+        ),
+      );
       notifyListeners();
     } catch (e) {
-      throw (e);
+      print(e);
+    }
+  }
+
+  Future<void> updateProducts(Map data) async {
+    try {
+      final prodIndex = _items.indexWhere((prod) => prod.id == data["id"]);
+      const url = "http://10.0.2.2:5100/product";
+      // const url = "https://acc6d527438b.ngrok.io/product";
+      var added = await http.put(
+        url,
+        body: json.encode(
+          {...data},
+        ),
+      );
+
+      final extractedData = json.decode(added.body) as Map<String, dynamic>;
+      _items[prodIndex] = Product(
+        id: extractedData["id"],
+        price: extractedData["price"],
+        title: extractedData["title"],
+        description: extractedData["description"],
+        imageUrl: extractedData["imageUrl"],
+      );
+      notifyListeners();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> deleteProducts({String id}) async {
+    final url = 'http://10.0.2.2:5100/product/$id';
+    // final url = 'https://acc6d527438b.ngrok.io/product/$id';
+    final prodIndex = _items.indexWhere((prod) => prod.id == id);
+    final existingProduct = _items[prodIndex];
+    _items.removeAt(prodIndex);
+    notifyListeners();
+    var res = await http.delete(url);
+    if (res.statusCode >= 300) {
+      _items.insert(prodIndex, existingProduct);
+      notifyListeners();
+      throw HttpException("Could not delete product");
     }
   }
 }
--- lib/widget/productView.dart
+++ lib/widget/productView.dart
@@ -1,13 +1,17 @@
 import "package:flutter/material.dart";
+import 'package:stateManagement/provider/products.dart';
 import '../screen/productDetail.dart';
 import "package:provider/provider.dart";
 import "../provider/product.dart";
+import "../provider/products.dart";
 import "../provider/cart.dart";
 
 class ProductView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final scaffold = Scaffold.of(context);
     final product = Provider.of<Product>(context, listen: false);
+    final products = Provider.of<Products>(context, listen: false);
     final cartData = Provider.of<Cart>(context, listen: false);
     return GestureDetector(
       onTap: () {
@@ -26,15 +30,31 @@ class ProductView extends StatelessWidget {
           ),
           footer: GridTileBar(
             backgroundColor: Colors.black54,
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
+            title: GestureDetector(
+              onTap: () async {
+                try {
+                  await products.deleteProducts(id: product.id);
+                  print("pass");
+                } catch (e) {
+                  print("error");
+                  scaffold.showSnackBar(
+                    SnackBar(
+                      content: Text('deleting Failed', textAlign: TextAlign.center,),
+                    ),
+                  );
+                }
+              },
+              child: Text(
+                product.title,
+                textAlign: TextAlign.center,
+              ),
             ),
             leading: Consumer<Product>(
               builder: (ctx, product, child) => IconButton(
                 icon: Icon(
                     product.favorite ? Icons.favorite : Icons.favorite_border),
                 onPressed: () {
+                  products.addProduct();
                   product.addToFavorite();
                 },
               ),
@@ -42,6 +62,16 @@ class ProductView extends StatelessWidget {
             trailing: IconButton(
               icon: Icon(Icons.shopping_cart),
               onPressed: () {
+                products.updateProducts({
+                  "id": product.id,
+                  "title": product.title,
+                  "description": product.description,
+                  "price": product.price,
+                  "imageUrl":
+                      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg"
+                  // product.imageUrl,
+                });
+
                 cartData.addItems(
                   productId: product.id,
                   title: product.title,
